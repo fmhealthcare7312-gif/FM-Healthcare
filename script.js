e885225c5f1998db0a22009836503a84f3286ee3
fmhealthcare7312-gif <fmhealthcare7312@gmail.com>
2026-07-08 15:00:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,22 +2,6 @@
 
 console.log("FM Healthcare Website Loaded");
 
-// Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-
-        const target = document.querySelector(this.getAttribute("href"));
-
-        if(target){
-            target.scrollIntoView({
-                behavior: "smooth"
-            });
-        }
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to FM Healthcare");
 });
-
-// Welcome Message
-window.onload = function () {
-    console.log("Welcome to FM Healthcare");
-};
