2c5305f528cceec7e2d0ceb4fd9e6c1123887c5a
fmhealthcare7312-gif <fmhealthcare7312@gmail.com>
2026-07-17 14:04:27 +0530

    Added Test Price Script Loader

--- script.js
+++ script.js
@@ -5,3 +5,54 @@ console.log("FM Healthcare Website Loaded");
 document.addEventListener("DOMContentLoaded", function () {
     alert("Welcome to FM Healthcare");
 });
+/* ===============================
+   TEST PRICE LOADER
+================================ */
+
+async function loadTestPrices() {
+
+    const response = await fetch("data/test-price.json");
+
+    const data = await response.json();
+
+    const tableBody = document.getElementById("tableBody");
+
+    const searchInput = document.getElementById("searchInput");
+
+    function render(list){
+
+        tableBody.innerHTML="";
+
+        list.forEach(item=>{
+
+            tableBody.innerHTML += `
+                <tr>
+                    <td>${item.test}</td>
+                    <td>${item.report}</td>
+                    <td>₹${item.price}</td>
+                </tr>
+            `;
+
+        });
+
+    }
+
+    render(data);
+
+    searchInput.addEventListener("keyup",()=>{
+
+        const value = searchInput.value.toLowerCase();
+
+        const filtered = data.filter(item=>
+
+            item.test.toLowerCase().includes(value)
+
+        );
+
+        render(filtered);
+
+    });
+
+}
+
+loadTestPrices();
